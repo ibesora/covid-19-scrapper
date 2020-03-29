4eec12a91aa051b5c0498ffc040e5c09cdcd68d5
ibesora <isaac.besora@gmail.com>
2020-03-29 18:48:36 +0200

    Adds move rule to move data files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
-const gulp = require('gulp');
-const flowRemoveTypes = require('gulp-flow-remove-types');
-const babel = require('gulp-babel');
+// @flow
+const gulp = require("gulp");
+const flowRemoveTypes = require("gulp-flow-remove-types");
+const babel = require("gulp-babel");
 
-gulp.task('build', function () {
-    return gulp.src('./src/**/*.js')
-        .pipe(flowRemoveTypes({pretty: true}))
-        .pipe(babel({plugins: ['babel-plugin-unassert']}))
-        .pipe(gulp.dest('./dist/'));
+gulp.task("build", () => {
+
+	return gulp.src("./src/**/*.js")
+		.pipe(flowRemoveTypes({pretty: true}))
+		.pipe(babel({plugins: ["babel-plugin-unassert"]}))
+		.pipe(gulp.dest("./dist/"));
+
 });
+
+gulp.task("move", () => {
+
+	return gulp.src("./src/**/*.json")
+		.pipe(gulp.dest("./dist/"));
+
+})
+;
