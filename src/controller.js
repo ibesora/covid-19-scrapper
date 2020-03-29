b248e46940b6af5a7d6cece50935181f1d7aae75
ibesora <isaac.besora@gmail.com>
2020-03-29 10:58:45 +0200

    Exports geojson file

--- src/controller.js
+++ src/controller.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const moment = require("moment");
 const Scrapper = require("./scrapper");
 const DataConverter = require("./dataConverter");
+const areesBasiquesGeom = require("./areesBasiquesGeom.json");
 
 const exportObjectToCSV = (input) => {
 
@@ -25,6 +26,50 @@ const exportObjectToCSV = (input) => {
 
 };
 
+const exportObjectToGeoJSON = (input) => {
+
+	const inputById = input.reduce((agg, el) => {
+
+		agg[el.id] = el; return agg;
+
+	}, {});
+	const geomById = areesBasiquesGeom.features.reduce((agg, el) => {
+
+		agg[el.properties.CODIABS] = el; return agg;
+
+	}, {});
+
+	const geojson = {
+		type: "FeatureCollection",
+		name: "areesBasiques",
+		crs: { type: "name", properties: { name: "urn:ogc:def:crs:EPSG::25831" } },
+		features: []
+	};
+
+	Object.keys(inputById).forEach(key => {
+
+		const current = inputById[key];
+		const geom = geomById[key];
+		geojson.features.push({
+			type: "Feature",
+			properties: {
+				...current,
+				CODISS: geom.properties.CODISS,
+				NOMSS: geom.properties.NOMSS,
+				CODIRS: geom.properties.CODIRS,
+				NOMRS: geom.properties.NOMRS,
+				CODIAGA: geom.properties.CODIAGA,
+				NOMAGA: geom.properties.NOMAGA,
+			},
+			geometry: geom.geometry
+		});
+
+	});
+
+	return geojson;
+
+};
+
 module.exports = {
 	fetchAndSaveAreesBasiques: async () => {
 
@@ -32,6 +77,7 @@ module.exports = {
 		const ABS = DataConverter.convertAreesBasiquesFromResponse(ABSResponse);
 		fs.writeFileSync(`./data/${moment().format("YYYY-MM-DD")}-areesBasiques.json`, JSON.stringify(ABS));
 		fs.writeFileSync(`./data/${moment().format("YYYY-MM-DD")}-areesBasiques.csv`, exportObjectToCSV(ABS));
+		fs.writeFileSync(`./data/${moment().format("YYYY-MM-DD")}-areesBasiques.geojson`, JSON.stringify(exportObjectToGeoJSON(ABS)));
 
 	},
 	fetchAndSavePCRTotals: async () => {
